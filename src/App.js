eebddd491b074ec8995798222d32457ed13da145
Guppi334 <st0910na0401@gmail.com>
2020-06-09 10:12:58 +0900

    learn components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-
-class App extends Component{
-  render() {
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked")}}></input>
-      </React.Fragment>
-    )
-  }
-}
+import React from 'react';
 
 // function App() {
-//   return  <h1>Hello World!</h1>;
+//   return (
+//     <div>
+//         <h1>
+//           Hello World
+//         </h1>
+//     </div>
+//   );
 // }
+
+const App = () => {
+  return (
+  <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+  </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
 
 export default App;

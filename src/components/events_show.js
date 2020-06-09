90c0cc1f79d4cd90294a57d83cd155af55aec226
Guppi334 <st0910na0401@gmail.com>
2020-06-09 14:56:40 +0900

    implement event eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,11 @@ class EventsShow extends Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.onDeleteClick =this.onDeleteClick.bind(this)
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.props.getEvent(id)
+  }
   renderField(field) {
     const {input, label, type, meta: {touched, error } } = field
 
@@ -23,7 +28,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -34,8 +39,7 @@ class EventsShow extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
-    console.log(submitting)
+    const { handleSubmit, pristine, submitting, invalid } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -44,7 +48,7 @@ class EventsShow extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting } />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid } />
           <Link to="/" >Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
@@ -62,8 +66,13 @@ const validate = values => {
   return errors
 }
 
-const mapDispachToProps = ({ deleteEvent })
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, state }
+}
 
-export default connect(null, mapDispachToProps)(
-  reduxForm ({ validate, form: 'eventShowForm'})(EventsShow)
+const mapDispachToProps = ({ deleteEvent, getEvent, putEvent })
+
+export default connect(mapStateToProps, mapDispachToProps)(
+  reduxForm ({ validate, form: 'eventShowForm', enableReinitialize: true})(EventsShow)
 )

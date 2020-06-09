9b92eae3f041dc8604c989ed1428733b9f0f73cd
Guppi334 <st0910na0401@gmail.com>
2020-06-09 10:10:45 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-        <h1>
-          Hello World
-        </h1>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}}></input>
+      </React.Fragment>
+    )
+  }
 }
+
+// function App() {
+//   return  <h1>Hello World!</h1>;
+// }
 
 export default App;

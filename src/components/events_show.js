dea68857786820a9803a4d3a933ccc9b6705ebd4
Guppi334 <st0910na0401@gmail.com>
2020-06-09 14:12:36 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { postEvent } from '../actions'
-import { Link } from 'react-router-dom'
-import { Field, reduxFor, reduxForm} from 'redux-form'
+import { getEvent, deleteEvent, putEvent } from '../actions';
+
+import { Link } from 'react-router-dom';
+import { Field, reduxFor, reduxForm} from 'redux-form';
 
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick =this.onDeleteClick.bind(this)
   }
   renderField(field) {
     const {input, label, type, meta: {touched, error } } = field
@@ -21,12 +23,19 @@ class EventsNew extends Component {
   }
 
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    // await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
+  async onDeleteClick() {
+    const { id } = this.props.match.params
+    await this.props.deleteEvent(id)
     this.props.history.push('/')
   }
 
   render() {
     const { handleSubmit, pristine, submitting } = this.props
+    console.log(submitting)
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -37,6 +46,7 @@ class EventsNew extends Component {
         <div>
           <input type="submit" value="Submit" disabled={pristine || submitting } />
           <Link to="/" >Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
       </form>
     )
@@ -52,8 +62,8 @@ const validate = values => {
   return errors
 }
 
-const mapDispachToProps = ({ postEvent })
+const mapDispachToProps = ({ deleteEvent })
 
 export default connect(null, mapDispachToProps)(
-  reduxForm ({ validate, form: 'eventNewForm'})(EventsNew)
+  reduxForm ({ validate, form: 'eventShowForm'})(EventsShow)
 )
